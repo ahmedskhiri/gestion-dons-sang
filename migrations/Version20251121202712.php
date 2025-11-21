3c2de85713e321b1032799a38e63e3437c51e021
slimaissa-pixel <slimtchiken@gmail.com>
2025-11-21 21:32:16 +0100

    test

--- migrations/Version20251121202712.php
+++ migrations/Version20251121202712.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251120233443 extends AbstractMigration
+final class Version20251121202712 extends AbstractMigration
 {
     public function getDescription(): string
     {
